010c0c51af152386255f11cdb08c24cdf748c527
Shivam <shivamch0865@gmail.com>
2026-04-04 15:06:34 +0530

    Mini Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import AddToQueue from "./components/AddToQueue"
 import CurrentQueue from "./components/CurrentQueue"
 import "./App.css"
+import { useState } from "react"
 
 
 function App() {
 
+  const [queue , setQueue] = useState([])
+
+  const addToQueue = (customer) => {
+    setQueue([...queue , {...customer , id : Date.now() , status: "waiting"}])
+  }
+
+   const updateStatus = (id, newStatus) => {
+    setQueue(queue.map(customer => {
+      return customer.id === id ? {...customer , status : newStatus} : customer
+    } ))
+  }
+
+  const removeFromQueue = (id) => {
+    setQueue(queue.filter(customer => customer.id !== id))
+  }
 
   return (
     <>
     <div className="min-h-screen bg-black text-white text-center pl-60 pr-60 pt-1">
       <h1 className="text-5xl font-bold mt-30 mb-10">Queue Management System</h1>
       <p className="text-2xl text-gray-500">Manage your customers efficiently</p>
-        <div className="flex gap-20 mt-10">
-          <AddToQueue />
-          <CurrentQueue />
-        </div>
+        <main className="flex gap-20 mt-10">
+          <AddToQueue onAdd={addToQueue}/>
+          <CurrentQueue queue={queue} onUpdateStatus={updateStatus} onRemove={removeFromQueue} />
+        </main>
     </div>
     </>
   )
--- src/components/AddToQueue.jsx
+++ src/components/AddToQueue.jsx
@@ -1,19 +1,32 @@
 import React , { useState } from 'react'
 
-function AddToQueue() {
-    const [] = useState();
+function AddToQueue({onAdd}) {
+    const [name , setName] = useState("");
+    const [service , setService] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!name.trim() || !service.trim()) return;
+
+        onAdd({name , service});
+        setName('');
+        setService('')
+    }
   return (
-     <form className="queue-form">
+     <form className="queue-form" onSubmit={handleSubmit}>
             <h2 className='text-2xl font-bold'>Add to queue</h2>
             <div className="form-group">
               <input
                 placeholder="Customer name"
-               
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 type="text"
               />
             </div>
             <div className="form-group">
-              <select >
+              <select value={service} onChange={(e) => setService(e.target.value)}>
                 <option value="">Select Service</option>
                 <option value="consultation">consultation</option>
                 <option value="payment">payment</option>
